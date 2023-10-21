1fb90cff4b01b3a11ed31e4f8c46a164cc3a501a
Lawrence Caulker <locaulker@gmail.com>
2023-10-21 16:35:26 -0400

    implemented custom error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,27 @@
+import Wrapper from "../assets/wrappers/ErrorPage"
+import { Link, useRouteError } from "react-router-dom"
+import img from "../assets/not-found.svg"
+
 const Error = () => {
-  return <h1>Error</h1>
+  const error = useRouteError()
+  console.log(error)
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="Image Not Found" />
+          <h3 style={{ color: "red" }}>Oops!</h3>
+          <p>Page NOT found</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    )
+  }
+
+  return (
+    <Wrapper>
+      <h3>Something went wrong!</h3>
+    </Wrapper>
+  )
 }
 export default Error
