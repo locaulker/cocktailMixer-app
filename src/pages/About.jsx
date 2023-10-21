a8d4768700ed9c79eab8aae889085aaedb59f02d
Lawrence Caulker <locaulker@gmail.com>
2023-10-21 02:03:13 -0400

    implemented the Link component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom"
+
 const About = () => {
-  return <h1>About</h1>
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home Page</Link>)
+    </div>
+  )
 }
 export default About
