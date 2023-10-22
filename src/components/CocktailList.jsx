5d084bf54774d0b2d5212312f6a1e1c09a495d5b
Lawrence Caulker <locaulker@gmail.com>
2023-10-22 04:58:07 -0400

    added list of Cocktail cards

---
+++ src/components/CocktailList.jsx
@@ -0,0 +1,27 @@
+import Wrapper from "../assets/wrappers/CocktailList"
+import CocktailCard from "./CocktailCard"
+
+const CocktailList = ({ drinks }) => {
+  if (!drinks) {
+    return <h4 style={{ textAlign: "center" }}>No Matching Cocktail...</h4>
+  }
+  const formattedDrinks = drinks.map((item) => {
+    const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item
+    return {
+      id: idDrink,
+      name: strDrink,
+      image: strDrinkThumb,
+      info: strAlcoholic,
+      glass: strGlass
+    }
+  })
+
+  return (
+    <Wrapper>
+      {formattedDrinks.map((drink) => {
+        return <CocktailCard key={drink.id} {...drink} />
+      })}
+    </Wrapper>
+  )
+}
+export default CocktailList
