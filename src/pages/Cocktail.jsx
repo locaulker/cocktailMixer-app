1bd67b73cf169deebe1ab913593ef83d53029cbf
Lawrence Caulker <locaulker@gmail.com>
2023-10-23 06:57:31 -0400

    rendered single cocktail

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -14,7 +14,50 @@ export const loader = async ({ params }) => {
 
 const Cocktail = () => {
   const { id, data } = useLoaderData()
+  const singleDrink = data.drinks[0]
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions
+  } = singleDrink
 
-  return <h1>Cocktail</h1>
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">Name:</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">Category:</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">Info:</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">Glass:</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">Instructions:</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 export default Cocktail
